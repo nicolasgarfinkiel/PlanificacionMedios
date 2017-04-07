48f38f7093159dff23d9077ff12c442270bf9951
pablozabo <pablozabo@hotmail.com>
2017-04-07 11:40:29 -0300

    Validaciones tarifas

--- Irsa.PDM.MainWebApp/Scripts/app/tarifas/tarifasEditCtrl.js
+++ Irsa.PDM.MainWebApp/Scripts/app/tarifas/tarifasEditCtrl.js
@@ -5,33 +5,22 @@
            'tarifasService',
            'baseNavigationService',
            'editBootstraperService',
-           function ($scope, $routeParams, tarifasService, baseNavigationService, editBootstraperService) {               
+           function ($scope, $routeParams, tarifasService, baseNavigationService, editBootstraperService) {
+               $scope.dias = ['Lunes', 'Martes', 'Miercoles', 'Juves', 'Viernes', 'Sabado', 'Domingo'];
+               $scope.tarifaInit = { lunes: true, martes: true, miercoles: true, jueves: true, viernes: true, sabado: true, domingo: true };
+               $scope.tarifas = [angular.copy($scope.tarifaInit)];
+
                //#region base
 
-               $scope.onInitEnd = function () {                   
+               $scope.onInitEnd = function () {
                };
 
-               editBootstraperService.init($scope, $routeParams,  {
+               editBootstraperService.init($scope, $routeParams, {
                    service: tarifasService,
                    navigation: baseNavigationService
                });
-              
-               $scope.isValid = function() {
-                   $scope.result = { hasErrors: false, messages: [] };                   
-
-                   //if (!$scope.entity.nombre) {
-                   //    $scope.result.messages.push('Ingrese el nombre');
-                   //}                  
-               
-
-                   $scope.result.hasErrors = $scope.result.messages.length;
-                   return !$scope.result.hasErrors;
-               };
 
                //#endregion
-
-               $scope.dias = ['Lunes', 'Martes', 'Miercoles', 'Juves', 'Viernes', 'Sabado', 'Domingo'];
-               $scope.tarifas = [{}];
 
                //#region filter
 
@@ -44,7 +33,7 @@
                    vehiculos: [],
                    diasAux: [],
                    dias: []
-               };              
+               };
 
                $scope.toggleCheck = function (entity, id) {
                    if ($scope.filter[entity + 'Aux'].indexOf(id) === -1) {
@@ -54,7 +43,7 @@
                    }
                };
 
-               $scope.setAux = function(entity) {
+               $scope.setAux = function (entity) {
                    $scope.filter[entity + 'Aux'] = angular.copy($scope.filter[entity]);
                };
 
@@ -70,11 +59,11 @@
                };
 
                $scope.setAuxHoras = function (entity) {
-                   $scope.filter[entity + 'Aux'] = $scope.filter[entity];                   
+                   $scope.filter[entity + 'Aux'] = $scope.filter[entity];
                };
 
                $scope.applyFilterHoras = function (entity) {
-                   $scope.filter[entity] = $scope.filter[entity + 'Aux'];                   
+                   $scope.filter[entity] = $scope.filter[entity + 'Aux'];
                };
 
                $scope.clearFilterHoras = function (entity, $event) {
@@ -83,14 +72,80 @@
                    $event.stopPropagation();
                };
 
-               //#endregion
+               //#endregion                      
 
-               $scope.findTarifas = function() {
-                   
+               $scope.findTarifas = function () {
+
                };
 
-               $scope.addTarifa = function(current) {
-                   var index = $scope.tarifas.indexOf(current);
-                   $scope.tarifas.splice(index +1, 0, {});
+               $scope.addTarifa = function (current) {
+                   $scope.validateAndSave(function() {
+                       var index = $scope.tarifas.indexOf(current);
+                       var tarifa = angular.copy($scope.tarifaInit);
+                       $scope.tarifas.splice(index + 1, 0, tarifa);
+                       $scope.currentTarifa = null;
+                       $scope.setEditable(tarifa);
+                   });                   
                };
+
+               $scope.deleteTarifa = function (tarifa) {
+                   if (tarifa == $scope.currentTarifa) {
+                       var index = $scope.tarifas.indexOf(tarifa);
+                       $scope.tarifas.splice(index, 1);
+                       $scope.currentTarifa = null;
+                   } else {
+                       $scope.validateAndSave(function () {
+                           var index = $scope.tarifas.indexOf(tarifa);
+                           $scope.tarifas.splice(index, 1);
+                           $scope.currentTarifa = null;
+                       });
+                   }                   
+               };
+
+               $scope.setEditable = function (tarifa) {                   
+                   $scope.validateAndSave(function () {                                          
+                       if (tarifa == $scope.currentTarifa) {
+                           $scope.currentTarifa = null;
+                           return;
+                       }
+
+                       $scope.currentTarifa = tarifa;
+                       $scope.currentTarifa.editable = !$scope.currentTarifa.editable;                       
+                   });                   
+               }
+
+               $scope.validateAndSave = function (callback) {
+                   if ($scope.currentTarifa == null) {
+                       callback();
+                       return;
+                   }
+
+                   if (!$scope.isValidTarifa($scope.currentTarifa)) return;
+
+                   //TODO: save
+                   $scope.currentTarifa.editable = false;
+                 //  $scope.currentTarifa = null;
+                   callback();
+               }
+
+               $scope.isValidTarifa = function (tarifa) {
+                   tarifa.invalidMedio = !tarifa.medio;
+                   tarifa.invalidPlaza = !tarifa.plaza;
+                   tarifa.invalidVehiculo = !tarifa.vehiculo;
+                   tarifa.invalidDias = !tarifa.lunes && !tarifa.martes && !tarifa.miercoles && !tarifa.jueves && !tarifa.viernes && !tarifa.sabado && !tarifa.domingo;
+                   tarifa.invalidHoraDesde = !tarifa.horaDesde;
+                   tarifa.invalidHoraHasta = !tarifa.horaHasta;
+                   tarifa.invalidTarifa = !tarifa.importe || isNaN(tarifa.importe);
+                   tarifa.invalidDescripcion = !tarifa.descripcion;
+
+                   return !(tarifa.invalidMedio ||
+                       tarifa.invalidPlaza ||
+                       tarifa.invalidVehiculo ||
+                       tarifa.invalidDias ||
+                       tarifa.invalidHoraDesde ||
+                       tarifa.invalidHoraHasta ||
+                       tarifa.invalidTarifa ||
+                       tarifa.invalidDescripcion);
+               }
+             
            }]);

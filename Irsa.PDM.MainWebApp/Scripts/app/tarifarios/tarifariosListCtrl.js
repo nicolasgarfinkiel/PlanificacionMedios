154a6ee68c3c0f742e110fe4d347f43d77c0df08
pablozabo <pablozabo@hotmail.com>
2017-04-12 23:25:07 -0300

    Avances abm tarifarios

--- Irsa.PDM.MainWebApp/Scripts/app/tarifarios/tarifariosListCtrl.js
+++ Irsa.PDM.MainWebApp/Scripts/app/tarifarios/tarifariosListCtrl.js
@@ -5,6 +5,9 @@
            'baseNavigationService',
            'listBootstraperService',
            function ($scope, tarifariosService, baseNavigationService, listBootstraperService) {
+
+               //#region Base
+
                $scope.onInitEnd = function () {                          
                };
 
@@ -15,7 +18,68 @@
                        { field: 'id', displayName: 'Id'},
                        { field: 'fechaDesde', displayName: 'Fecha desde' },
                        { field: 'fechaHasta', displayName: 'Fecha hasta' },
-                       { field: 'cuit', displayName: 'Acciones', width: 80, cellTemplate: '<div class="ng-grid-icon-container"><a href="javascript:void(0)" class="btn btn-rounded btn-xs btn-icon btn-default" ng-click="edit(row.entity.id)"><i class="fa fa-pencil"></i></a></div>' }
+                       { field: 'cuit', displayName: 'Acciones', width: 80, cellTemplate: '<div class="ng-grid-icon-container"><a href="javascript:void(0)" class="btn btn-rounded btn-xs btn-icon btn-default" ng-click="setEdit(row.entity)"><i class="fa fa-pencil"></i></a></div>' }
                    ]
                });
+
+               //#endregion
+
+               $scope.resultModal = { hasErrors: false, messages: [] };
+
+               $scope.setCreate = function () {
+                   $scope.resultModal.hasErrors = false;
+                   $scope.operation = 'Alta';
+                   $scope.entity = {};
+                   $('#tarifarioModal').modal('show');
+               };
+
+               $scope.setEdit = function (entity) {                   
+                   $scope.resultModal.hasErrors = false;
+                   $scope.operation = 'Edición';
+                   $scope.entity = angular.copy(entity);
+                   $('#tarifarioModal').modal('show');
+               };
+
+               $scope.save = function () {
+                   if (!$scope.isValid()) return;
+
+                   if ($scope.entity.id) {
+                       tarifariosService.updateEntity($scope.entity).then(function (response) {
+                           if (!response.data.result.hasErrors) {
+                               $scope.find();                               
+                               $('#tarifarioModal').modal('hide');
+                               return;
+                           }
+
+                           $scope.resultModal = response.data.result;
+                       }, function () { throw 'Error on updateUsuarioCliente'; });
+
+                   } else {                       
+                       tarifariosService.createEntity($scope.entity).then(function (response) {
+                           if (!response.data.result.hasErrors) {
+                               $scope.find();                               
+                               $('#tarifarioModal').modal('hide');
+                               return;
+                           }
+
+                           $scope.resultModal = response.data.result;
+                       }, function () { throw 'Error on createCliente'; });
+                   }
+               };
+
+               $scope.isValid = function () {
+                   $scope.resultModal.hasErrors = false;
+                   $scope.resultModal.messages = [];                   
+
+                   if (!$scope.entity.fechaDesde) {
+                       $scope.resultModal.messages.push('Ingrese la fecha desde');
+                   }
+
+                   if (!$scope.entity.fechaHasta) {
+                       $scope.resultModal.messages.push('Ingrese la fecha hasta');
+                   }
+
+                   $scope.resultModal.hasErrors = $scope.resultModal.messages.length;
+                   return !$scope.resultModal.hasErrors;
+               };
            }]);

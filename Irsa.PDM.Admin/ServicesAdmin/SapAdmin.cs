459f31d8452e10d0f67466cdd2aefd086356a904
pablozabo <pablozabo@hotmail.com>
2017-10-09 23:49:16 -0300

    Primeros pasos ws consumo

--- Irsa.PDM.Admin/ServicesAdmin/SapAdmin.cs
+++ Irsa.PDM.Admin/ServicesAdmin/SapAdmin.cs
@@ -27,14 +27,57 @@ namespace Cresud.CDP.Admin.ServicesAdmin
 //            service.clie
 
             #endregion
-            
+
+            var request = new DT_PDM_Consumos_In_Request
+            {
+                ZMMIM_CONMED_F001 = new List<DT_PDM_Consumos_In_RequestItem>
+                {
+                    new DT_PDM_Consumos_In_RequestItem
+                    {
+                        bank = "",
+                        documentHeaderText = "",
+                        idConsume  = "",
+                        materialNumber = "",
+                        plant = "",
+                        quantity = "",
+                        storageLocation = ""
+                    }
+                }.ToArray(),
+                ZMMIM_CONMED_F002 = new List<DT_PDM_Consumos_In_RequestItem1>
+                {
+                    new DT_PDM_Consumos_In_RequestItem1
+                    {
+                        bank = "",
+                        documentHeaderText = "",
+                        idConsume  = "",
+                        materialNumber = "",
+                        plant = "",
+                        quantity = "",
+                        storageLocation = "",
+                        orderNumber = ""
+                    }
+                }.ToArray(),
+                ZMMIM_CONMED_F003 = new List<DT_PDM_Consumos_In_RequestItem2>
+                {
+                    new DT_PDM_Consumos_In_RequestItem2
+                    {
+                        bank = "",
+                        documentHeaderText = "",
+                        idConsume  = "",
+                        materialNumber = "",                        
+                        quantity = "",
+                        orderNumber = "",
+                        plant_D = "",
+                        
+                        
+                    }
+                }.ToArray(), 
+               
+            };
 
            service.SI_PDM_Consumos_In_Request(new DT_PDM_Consumos_In_Request());
 
-           service.SI_PDM_Consumos_In_RequestCompleted += (sender, args) =>
-           {
-               var j = 0;
-           };
+          
             
         }    
     }
